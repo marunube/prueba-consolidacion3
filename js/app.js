18500febf608de6446f1573ac146c3d9a8787700
marunube <martinariquelmevilla@gmail.com>
2023-07-16 23:59:20 -0400

    add viewpoint css

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function collapsable () {
+    console.log("clickeado :D");
+
+    var element = document.getElementsByClassName("chat__user");
+    element[0].classList.toggle("chat_translated");
+}
